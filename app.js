11c89e6d88e9d67e158a559ec3c4e6111815f096
hkageyama <38199636+hkageyama@users.noreply.github.com>
2018-04-18 20:48:38 +0900

    Initial Commit

---
+++ app.js
@@ -0,0 +1,65 @@
+// "./node_modules/.bin/nodemon" app.js を実行しておくと、ソース修正後にオートコンパイル
+// 各種import
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+
+// 各種定数
+const PORT = process.env.PORT || 3000;
+
+// オブジェクト生成
+var Chat_server = require('./lib/chat_server');
+var chat_server = new Chat_server();
+var Access_db = require('./lib/access_db');
+var access_db = new Access_db();
+
+// mySQL接続プール設定
+var pool = mysql.createPool({
+  connectionLimit : 10,
+  host     : 'localhost',
+  user     : 'root',
+  password : 'admin',
+  database : 'exchat'
+});
+
+// ejs(テンプレ)を使用するための設定
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+// postデータ(json)を取得する為の設定
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// コンソールにデバッグ情報を表示
+app.use(logger('dev'));
+// publicフォルダにパスを通す（expressではこれをmiddlewareの適用と呼ぶ）
+// http://localhost:3000/【publicフォルダ配下のファイル名】で 指定ファイルがリダイレクトされる
+app.use(express.static(__dirname + '/views'));
+
+// agentチャット画面起動
+app.get('/agent', function(req, res) {
+  res.render('agent');
+});
+app.post('/agent', function(req, res) {
+  res.render('agent', {room_id: req.body.room_id});
+});
+// visitorチャット画面起動
+app.get('/visitor', function(req, res) {
+  res.render('visitor');
+});
+// 【テスト用】sdk向けpostリクエスト発行
+app.post('/post_to_sdk', function(req, res) {
+  console.log('send to SDK： ' + req.body.key);
+});
+
+// ポートlocalhost:3000にリスナー起動
+http.listen(PORT, () => {
+  console.log(`listening on *:${PORT}`);
+});
+
+// ソケット関連処理
+chat_server.listen(http, app);
+access_db.app(app);
---
+++ views/js/chat.js
@@ -0,0 +1,45 @@
+// var socket = io();
+var socket = io.connect('http://localhost:3000/',
+          { query: 'user_type=' + $('input:hidden[name="user_type"]').val()});
+// var socket = io.connect('http://localhost:3000/',
+//            { query: 'user_type=' + $('input:hidden[name="user_type"].val()'),
+//              query:  'room_id=' + $('input:hidden[name="room_id"].val()')});
+
+$(document).ready(function(){
+    //【agent】message receiption
+    socket.on('front chat message agent', (msg) => {
+        // $('#header-agent').empty();
+        // $('#header-agent').append('Test222!');
+        $('#messages-agent').append($('<li>').text(msg));
+        $('#messages-agent').scrollTop($('#messages-agent').prop('scrollHeight'));
+    });
+    //【visitor】message receiption
+    socket.on('front chat message visitor', (msg) => {
+        // $('#header-visitor').empty();
+        // $('#header-visitor').append('Test222!');
+        $('#messages-visitor').append($('<li>').text(msg));
+        $('#messages-visitor').scrollTop($('#messages-visitor').prop('scrollHeight'));
+    });
+    // 【agent】message send
+    $('#btn-send-agent').click(function() {
+        $('#form-agent').submit(function() {
+            // 1formにボタンが複数ある場合に起こるイベントの多重発生抑止。
+            $('#form-agent').off();
+            socket.emit('back chat message agent', $('#input-agent').val());
+            $('#input-agent').val('');
+            // イベント伝播防止(これがないと後続処理が流れない)
+            return false;
+        });
+    });
+    //【agent】chat done
+    $('#btn-done-agent').click(function() {
+        alert('done!');
+    });
+    //【visitor】message send
+    $('#form-visitor').submit(function(){
+        socket.emit('back chat message visitor', $('#input-visitor').val());
+        $('#input-visitor').val('');
+        // イベント伝播防止
+        return false;
+    });
+});
