25309c53c304544233684e89415ff3f79a631f65
hkageyama <38199636+hkageyama@users.noreply.github.com>
2018-05-01 19:02:06 +0900

    Initial Commit

--- views/js/chat.js
+++ views/js/chat.js
@@ -1,33 +1,29 @@
-// let chatSocket = io.connect('http://localhost:3000/chat/');
-let statusSocket = io.connect('http://localhost:3000/status/');
-
 const USER_TYPE_AGENT = 'agent'
 const USER_TYPE_VISITOR = 'visitor'
 
-const MESSAGES_OBJECT_NAME_HEADER = '#messages';
-const HEADER_OBJECT_NAME_HEADER = '#header';
-const ROOMID_OBJECT_NAME_HEADER = '#room_id';
-const FORM_OBJECT_NAME_HEADER = '#form';
-const INPUT_OBJECT_NAME_HEADER = '#input';
-const CONTENT_OBJECT_NAME_HEADER = '#content';
+const CONTENT_OBJECT_NAME_HEADER = '#content-';
+const MESSAGES_OBJECT_NAME = 'messages';
+const HEADER_OBJECT_NAME = 'header';
+const ROOMID_OBJECT_NAME = 'room_id';
+const INPUT_OBJECT_NAME = 'input';
 
 $(document).ready(function(){
     var baseRoomId = '';
 
     // メッセージ受信
     chatSocket.on('front chat message', (userType, value, roomId) => {
-        let domId = MESSAGES_OBJECT_NAME_HEADER + '-' + roomId;
-        $(domId).append($('<li>').text(value));
-        $(domId).scrollTop($(domId).prop('scrollHeight'));
+        let contentDomId = CONTENT_OBJECT_NAME_HEADER + roomId;
+        $(contentDomId).find('[name='+ MESSAGES_OBJECT_NAME + ']').append($('<li>').text(value));
+        $(contentDomId).find('[name='+ MESSAGES_OBJECT_NAME + ']').
+          scrollTop($(contentDomId).find('[name='+ MESSAGES_OBJECT_NAME + ']').prop('scrollHeight'));
     });
 
     // チャットウィジェット更新(HEADER/HIDDEN項目)
     chatSocket.on('front refresh', (roomId, userType) => {
-        let headerDomId = HEADER_OBJECT_NAME_HEADER + '-' + roomId;
-        let roomIdDomId = ROOMID_OBJECT_NAME_HEADER + '-' + roomId;
-        $(roomIdDomId).val(roomId);
-        $(headerDomId).empty();
-        $(headerDomId).append('Room # ' + roomId);
+        let contentDomId = CONTENT_OBJECT_NAME_HEADER + roomId;       
+        $(contentDomId).find('[name='+ ROOMID_OBJECT_NAME + ']').val(roomId);
+        $(contentDomId).find('[name='+ HEADER_OBJECT_NAME + ']').empty();
+        $(contentDomId).find('[name='+ HEADER_OBJECT_NAME + ']').append('Room # ' + roomId);
     });
 
     // チャットウィジェット 新規作成(エージェント) 
@@ -35,40 +31,62 @@ $(document).ready(function(){
         baseRoomId = openChatWidget(roomId, baseRoomId);
     });
 
-    // エージェントメッセージ送信
-    $('#btn_send-' + baseRoomId).click(function() {
-        var roomId =  getChildID($(this).attr("id"));
-        let domIdRoomId = ROOMID_OBJECT_NAME_HEADER + '-' + roomId;
-        let domIdInput = INPUT_OBJECT_NAME_HEADER + '-' + roomId;
+    // ルームリスト追加 
+    statusSocket.on('front append room list', (roomId, agentId) => {
+        $('#room_list').append(
+            '<div class="room_list_div" id="room_list_div-'  + roomId + '">' + roomId + ' : ' + agentId);
+    });
 
-        chatSocket.emit('back chat message agent', $(domIdRoomId).val(), $(domIdInput).val());
-        $(domIdInput).val('');
+    // ルームリスト追加 
+    statusSocket.on('front remove room list', (roomId) => {
+        $('#room_list_div-' + roomId).remove();
+    });
+
+    // エージェントメッセージ送信
+    $('[name=btn_send]').click(function() {
+        let roomId =  getChildID($(this).parent().parent().attr('id'));
+        if (roomId === '') return false;
+        let message = $(this).parent().parent().find('[name=' + INPUT_OBJECT_NAME + ']').val();
+        let contentDomId = CONTENT_OBJECT_NAME_HEADER + roomId;
+
+        chatSocket.emit('back chat message agent', roomId, message);
+        $(contentDomId).find('[name='+ INPUT_OBJECT_NAME + ']').val('');
     });
 
     // エージェントチャット画面クローズ
-    $('#btn_done-').click(function() {
-        // window.open('about:blank','_self').close();
-        // イベント伝播防止
-        return false;
+    $('[name=btn_done]').click(function() {
+        let obj = $(this).parent().parent()
+        let roomId =  getChildID(obj.attr('id'));
+        let contentDomId = CONTENT_OBJECT_NAME_HEADER + roomId;
+        if ($('.content').length > 1) {
+            $(contentDomId).remove();
+        } else {
+            // id初期化
+            clearDomObject(obj);
+            obj.attr("id", "content-");
+        }
+        chatSocket.emit('leave room', roomId);
     });
 
     // ルーム入室
-    $('#room_list div').on('click',function(){
+    $(document).on('click', '.room_list_div', function(){
         let roomInfo = $(this).text();
-        let roomId = roomInfo.substr(1, roomInfo.indexOf(':') - 1).trim();
-        if ($(ROOMID_OBJECT_NAME_HEADER + '-' +  roomId).val() != undefined) {
+        let roomId = roomInfo.substr(0, roomInfo.indexOf(':') - 1).trim();
+        let contentDomId = CONTENT_OBJECT_NAME_HEADER + roomId;
+        
+        // if ($(contentDomId).find('[name='+ ROOMID_OBJECT_NAME + ']').val() != undefined) {
+        if ($(contentDomId).attr('id') != undefined) {
             alert('Room#'+ roomId + 'には入室済みです。')
             return false;
         }
         // チャットウィジェットオープン(エージェント)
         baseRoomId = openChatWidget(roomId, baseRoomId);
-        chatSocket.emit('join room', roomId, USER_TYPE_AGENT);
-        return false;
+        chatSocket.emit('join room', roomId);
     });
 
     // ルーム作成（エージェント）
     $('#btn_create').click(function() {
-        chatSocket.emit('new room', USER_TYPE_AGENT);
+        chatSocket.emit('new room');
         // イベント伝播防止
         return false;
     });
@@ -79,15 +97,14 @@ $(document).ready(function(){
 
 // エージェントチャットウィジェット　オープン
 var openChatWidget = function(roomId, baseRoomId) {
-    let $obj;
-    if ($(ROOMID_OBJECT_NAME_HEADER + '-' + baseRoomId).val() === '' || 
-        $(ROOMID_OBJECT_NAME_HEADER + '-' + baseRoomId).val() === '/') {
-        obj = $(CONTENT_OBJECT_NAME_HEADER + '-' + baseRoomId);
-        setChatDomId(obj, roomId, baseRoomId);
+    let obj;
+    if ($(CONTENT_OBJECT_NAME_HEADER).attr('id') != undefined) {
+        obj = $(CONTENT_OBJECT_NAME_HEADER);
+        initChatDom(obj, roomId);
         baseRoomId = roomId;
     } else {
         obj = cloneChatDom(baseRoomId);
-        setChatDomId(obj, roomId, baseRoomId);
+        initChatDom(obj, roomId);
     }
 
     return baseRoomId;
@@ -96,29 +113,29 @@ var openChatWidget = function(roomId, baseRoomId) {
 // エージェントチャットウィジェットの複製
 var cloneChatDom = function(baseRoomId) {
     "use strict";
-    var $content = $(CONTENT_OBJECT_NAME_HEADER + '-' + baseRoomId);
+    var $content = $(CONTENT_OBJECT_NAME_HEADER + baseRoomId);
     return $content.clone(true).appendTo('#parent');
 }
 
-// フォーム複製後のオブジェクトID再設定
-var setChatDomId = function(target, roomId, baseRoomId) {
-
-    var increament_id = function(content, name, roomId, baseRoomId) {
-        var $obj = content.find('#' + name + baseRoomId);
-        $obj.attr('id', name + roomId);
-        if (name === 'header-') $obj.empty();
-        if (name === 'messages-') $obj.empty();
-        if (name === 'room_id-') $obj.val('');
-        if (name === 'input-') $obj.val('');
-    };
-    increament_id(target, 'header-', roomId, baseRoomId);
-    increament_id(target, 'messages-', roomId, baseRoomId);
-    increament_id(target, 'form-', roomId, baseRoomId);
-    increament_id(target, 'input-', roomId, baseRoomId);
-    increament_id(target, 'room_id-', roomId, baseRoomId);
-    increament_id(target, 'btn_send-', roomId, baseRoomId);
-    increament_id(target, 'btn_done-', roomId, baseRoomId);
+// チャットウィジェット初期化
+var initChatDom = function(target, roomId) {
+    clearDomObject(target);
     target.attr("id", "content-" + roomId);
+};
+
+// チャットウィジェット クリア
+var clearDomObject = function(target) {
+    var clearObject = function(content, name) {
+        var $obj = content.find('[name=' + name + ']');
+        if (name === 'header') $obj.empty();
+        if (name === 'messages') $obj.empty();
+        if (name === 'room_id') $obj.val('');
+        if (name === 'input') $obj.val('');
+    };
+    clearObject(target, 'header');
+    clearObject(target, 'messages');
+    clearObject(target, 'room_id');
+    clearObject(target, 'input');
 };
 
 // 子オブジェクトIDからルーム＃を取得する
